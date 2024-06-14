72d4e6d7b94a45fc69e814035e889671e97f49a6
Odumosu Bolaji <odmusbolaji31@gmail.com>
2024-06-14 21:21:18 +0100

    commit 4

---
+++ front-end/src/Components/Flashcard.jsx
@@ -0,0 +1,17 @@
+import './Quiz.css';
+import {useState} from 'react';
+function Flashcard(){
+
+    const [state, setState] = useState('Not start');
+    const [backcolor, setBackcolor] = useState('off');
+
+    return(
+        <>
+           <div className="box">
+           
+        </>
+    );
+
+}
+
+export default Flashcard;
