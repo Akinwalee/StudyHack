1972305fb1e5c7ff7fb78af66028f61148636e48
Obatula Fuad <obatulafuad@gmail.com>
2024-06-14 22:04:05 +0100

    Implement pdf handler

--- backend/utils/handle_pdf.py
+++ backend/utils/handle_pdf.py
@@ -1,3 +1,11 @@
 import fitz
 
 #I'll be using the PyMuPDF module to handle the pdf text extraction
+
+def extract_text(file):
+    doc = fitz.open(stream=file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    
+    return text
