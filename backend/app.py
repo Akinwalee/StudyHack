6df46269c8836f6112bd6a4ba5512619286cf1f1
Obatula Fuad <obatulafuad@gmail.com>
2024-06-14 21:06:06 +0100

    Restructure backend

--- backend/app.py
+++ backend/app.py
@@ -3,39 +3,11 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route('/form')
+@app.route('/')
 def index():
     return jsonify({"message": "StudyHack"})
-
-@app.route('/quiz')
-def quiz():
-    return jsonify({"message": "Study quizing"})
-
-@app.route("/flash")
-def flash():
-    return jsonify({"message": "This is a flash card"})
-
-#mcq
-def mcq():
-    pass
-    #mcq implementation
-
-#true false
-def boolean():
-    pass
-    #Implemetation for true or false
-
-#file upload
-def handleFile():
-    pass
-    #handler for file upload
-
-#text handler
-def handleText():
-    pass
-    #Handker for text input
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-    CORS(app)
+CORS(app)
