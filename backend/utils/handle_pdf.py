a23e79ac5b29f5cce362af00a0451a0aa03c7eea
Akinwalee <obatulafuad@gmail.com>
2024-06-15 15:18:17 +0100

    update pdf handler

--- backend/utils/handle_pdf.py
+++ backend/utils/handle_pdf.py
@@ -1,9 +1,9 @@
-import fitz
+import pymupdf
 
 #I'll be using the PyMuPDF module to handle the pdf text extraction
 
 def extract_text(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
+    doc = pymupdf.open(stream=file.read(), filetype="pdf")
     text = ""
     for page in doc:
         text += page.get_text()
