0bc51b6c851466d145be3659dbba488a2d953d1a
Akinwalee <obatulafuad@gmail.com>
2024-06-15 15:35:59 +0100

    update upload route

--- backend/routes/upload.py
+++ backend/routes/upload.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, jsonify
-
+from utils.handle_pdf import extract_text
 
 uploader = Blueprint('upload', __name__)
 
-#Route for text upload
+# Route for text upload
 @uploader.route("/text", methods=["POST"])
 def upload_text():
     data = request.get_json
@@ -11,12 +11,15 @@ def upload_text():
         return jsonify({"error": "Please input a text"}), 400
     return jsonify({"text": data[ "text" ]}), 200
 
+
 # Route for file upload
 @uploader.route("/file", methods=["POST"])
 def upload_file():
-    file = request.files['file']
+    if "file" not in request.files:
+        return jsonify({"error": "No file in request"})
+    file = request.files["file"]
     if file.filename == "":
         return jsonify({"error": "No file selected"}), 400
     if file:
-        text = extract_text(file) #extract_text() function is to be implemented seperately
+        text = extract_text(file)
         return jsonify({"text": text}), 200
