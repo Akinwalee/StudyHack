6dc235cb3d5ef836e2b523df7695515d8391ae45
Praise Aribisala <aribisalapraise12@gmail.com>
2024-06-16 09:17:02 +0100

    Changes made to dasboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,5 +1,6 @@
 
 import { useState, useRef } from "react";
+import { Link } from 'react-router-dom';
 import NavBar from "./NavBar";
 import './Dashboard.css';
 
@@ -66,11 +67,39 @@ export default function Dashboard() {
         }
     };
 
+    const uplaodText = async (text) => {
+        const formText = new FormData();
+        formText.append('text', text);
+
+        try {
+            const response = await fetch('http://localhost:5000/form', {
+                method: 'POST',
+                body: formText,
+            });
+
+            if (!response.ok) {
+                throw new Error('Text upload failed!');
+            }
+
+            const result = await response.json();
+            setUploadStatus(result.message);
+            console.log(result);
+        } catch (error) {
+            setUploadStatus(error.message);
+            console.error('Error uploading text:', error);
+        }
+    };
+
     const handleGenerateClick = () => {
-        if (file || text) {
+        if (file) {
             uploadFile(file);
+        } else if (text){
+            uplaodText(text);
         } else {
             setUploadStatus('Please select a file or paste text first.');
+            setTimeout(() => {
+                setUploadStatus('');
+            }, 3000); 
         }
     };
 
@@ -122,7 +151,7 @@ export default function Dashboard() {
                                     }}
                                     className="format"
                                 >
-                                    <option value="" disabled>Select format</option>
+                                    {/* <option value="" disabled>Select format</option> */}
                                     <option value="Quiz">Quiz</option>
                                     <option value="Flash Card">Flash Card</option>
                                 </select>
@@ -135,7 +164,7 @@ export default function Dashboard() {
                                     onChange={(e) => setSelectedQuestionType(e.target.value)}
                                     className="format"
                                 >
-                                    <option value="" disabled>Select question type</option>
+                                    {/* <option value="" disabled>Select question type</option> */}
                                     {selectedFormat === "Flash Card" ? (
                                         <>
                                             <option value="TF">T/F</option>
@@ -163,7 +192,7 @@ export default function Dashboard() {
                                     }}
                                     className="format"
                                 >
-                                    <option value="" disabled>Select difficulty</option>
+                                    {/* <option value="" disabled>Select difficulty</option> */}
                                     <option value="option1">Easy</option>
                                     <option value="option2">Medium</option>
                                     <option value="option3">Hard</option>
@@ -179,7 +208,7 @@ export default function Dashboard() {
                                     }}
                                     className="format"
                                 >
-                                    <option value="" disabled>Select question-count</option>
+                                    {/* <option value="" disabled>Select question-count</option> */}
                                     <option value="option1">5</option>
                                     <option value="option2">10</option>
                                     <option value="option3">15</option>
@@ -195,7 +224,14 @@ export default function Dashboard() {
                         </div>
                     </div>
                     <div className="submit-container">
-                        <div className="submit" onClick={handleGenerateClick}>Generate Now</div>
+                        <Link to={
+                            selectedFormat==='Quiz'
+                            ? '/quiz'
+                            : selectedFormat==='Flash Card'
+                            ? '/flashcard'
+                            : '/'
+                        } className="submit" onClick={handleGenerateClick}>Generate Now</Link>
+                        {/* <div className="submit" onClick={handleGenerateClick}>Generate Now</div> */}
                     </div>
                 </div>
             </div>
