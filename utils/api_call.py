b4212fad6031c3445c9738d75a0433bd91a54bc2
Akinwalee <obatulafuad@gmail.com>
2024-06-16 07:19:52 +0100

    Create response template

--- utils/api_call.py
+++ utils/api_call.py
@@ -3,6 +3,7 @@ import textwrap
 import google.generativeai as genai
 from IPython.display import display
 from IPython.display import Markdown
+from flask import jsonify
 API_KEY = None
 
 def to_markdown(text):
@@ -13,5 +14,14 @@ genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 
-def create_questions():
+def create_questions(text, assessment_type, question_type, difficulty, num_of_questions):
     pass
+
+def create_response(questions):
+    return jsonify({
+        "status_code": 20,
+        "mesaage": "Questions successfully generated",
+        "data": {
+            "questions": questions
+        }
+    })
