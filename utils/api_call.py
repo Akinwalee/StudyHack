f6a04ca1e05d3b1e24091c7d928c769c17fb2bc5
Akinwalee <obatulafuad@gmail.com>
2024-06-16 06:39:50 +0100

    setting up gemini API

--- utils/api_call.py
+++ utils/api_call.py
@@ -1,1 +1,12 @@
-# I would be restructuring this and create a folder for the gemini api in the app instead
+import pathlib
+import textwrap
+import google.gerativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
+
+def to_markdown(text):
+    text = text.replace("•", "  *")
+    return Markdown(textwrap.indent(text, "> ", predicate=lambda _: True))
+
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
