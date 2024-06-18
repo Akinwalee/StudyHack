8205b3962d2dc673a2cce1099cfe34fa6780ea10
Praise Aribisala <aribisalapraise12@gmail.com>
2024-06-18 23:05:07 +0100

    edited the front-end

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -21,15 +21,12 @@ function Quiz(){
     const [userInput, setUserInput] = useState('');
     const listItemRefs = useRef([]);
 
-    // const type = quizData["questions"]["questionIndex"]["type"]
     const questions = quizData.questions.map(q => q.question);
     const answers = quizData.questions.map(q => q.options);
     const correctAnswers = quizData.questions.map(q => q.correct_option || q.correct_answer);
-    // const format = quizData.questions[questionIndex].type
 
 
     const handleStart = () =>{
-        // console.log(quizData)
         setTimeout(() => {
             setQuizState("start");
         }, 500)
@@ -46,7 +43,6 @@ function Quiz(){
                 setUserInput('');
                 resetListItemStyles();
                 }, 500)
-            // setQuestionIndex((prev) => (prev + 1) % questions.length);
             
         }
         else{
@@ -56,9 +52,6 @@ function Quiz(){
             
         }
      
-        // setAnswered(false);
-        // setSelectedAnswer(null);
-        // resetListItemStyles();
     }
     const handlePrevQuestion = () =>{
         if (questionIndex > 0) {
@@ -69,7 +62,6 @@ function Quiz(){
                 setUserInput();
                 resetListItemStyles();
             }, 500)
-            // setQuestionIndex((prev) => (prev === 0 ? questions.length - 1 : prev - 1));
           
         }
         
@@ -94,7 +86,6 @@ function Quiz(){
             setSelectedAnswer(selectedId);
             setAnswered(true);
         }
-        // setSelectedAnswer(selectedAnswer)
         
         
     }
@@ -126,14 +117,6 @@ function Quiz(){
         
     };
 
-    // const resetListItemStyles = () => {
-    //     listItemRefs.current.forEach(li => {
-    //         if (li) {
-    //             li.style.backgroundColor = '';
-    //         }
-    //     });
-    // };
-
     const resetListItemStyles = () => {
         listItemRefs.current.forEach(li => {
             if (li) {
@@ -152,7 +135,6 @@ function Quiz(){
             navigate('/')
         }, 1000)
         
-        // console.log(format)
     }
 
     
@@ -166,7 +148,6 @@ function Quiz(){
                 <div className={`btn ${quizstate === "start" ? "start" : " "}`} onClick={handlePrevQuestion}>
                     <i className="fas fa-arrow-left"></i>Previous
                 </div>
-                {/* <div className={`btn counter ${quizstate === "start" ? "start" : " "}`}>{questionIndex + 1} / {questions.length}</div>  */}
                 <div className="screen">
                     {quizstate === 'not start' && (
                         <>
@@ -178,16 +159,12 @@ function Quiz(){
                     {quizstate === 'start' && (
                         <>
                             <div className={`question ${quizstate === "start" ? "start" : " "}`}>
-                            {/* <p className='score'>Score: {score}</p> */}
                             <p className="card-num">Question {questionIndex + 1} of {questions.length}</p>
                             <p className='quest'>{questions[questionIndex]}</p>
                             </div>
 
                             {quizData.questions[questionIndex].type.toLowerCase() === 'mcq' && (
                                 <div className={`answers ${quizstate === "start" ? "start" : " "}`}>
-                                    {/* {Object.keys(answers[questionIndex]).map((key) => (
-                                        <li key={key} onClick={() => handleCorrectAnswer(answers[questionIndex][key])} className={`${selectedAnswer === answers[questionIndex][key] ? (answers[questionIndex][key] === correctAnswers[questionIndex] ? "correct" : "incorrect") : ""}`}>{answers[questionIndex][key]}</li>
-                                    ))}; */}
                                     {answers[questionIndex].map((option, index) => (
                                         <li 
                                             key={index} 
@@ -203,9 +180,6 @@ function Quiz(){
 
                             {quizData.questions[questionIndex].type === 't/f' && (
                                 <div className={`answers ${quizstate === "start" ? "start" : " "}`}>
-                                    {/* {Object.keys(answers[questionIndex]).map((key) => (
-                                        <li key={key} onClick={() => handleCorrectAnswer(answers[questionIndex][key])} className={`${selectedAnswer === answers[questionIndex][key] ? (answers[questionIndex][key] === correctAnswers[questionIndex] ? "correct" : "incorrect") : ""}`}>{answers[questionIndex][key]}</li>
-                                    ))}; */}
                                     {answers[questionIndex].map((option, index) => (
                                         <li 
                                             key={index} 
@@ -249,15 +223,6 @@ function Quiz(){
                     
                 </div>
                 <div className={`btn ${quizstate === "start" ? "start" : " "}`} onClick={handleNextQuestion}>Next<i className="fas fa-arrow-right"></i></div>
-                {/* <div className={`finish ${quizstate === "start" ? "finish" : "hide"}`}>
-                    <p>You have completed the quiz, you scored {score}</p>
-                    <button className={`start-btn ${quizstate === "start" ? "hide" : " "}`} onClick={resetQuiz}>Start</button>
-                </div> */}
-                
-                {/* <div className={`score ${quizstate === "start" ? "start": ""}`}>Score: {score}</div> */}
-
-                
-            
             </div>
         </>
     );
