aefe2477f21a0a78ab3b9972923ab2a2ea499fc2
Akinwalee <obatulafuad@gmail.com>
2024-06-19 01:17:37 +0100

    Create quiz comment route

--- routes/feedbacks.py
+++ routes/feedbacks.py
@@ -3,15 +3,15 @@ from utils.api_call import feedback_flash, feedback_quiz
 
 feedbacks = Blueprint('feedback', __name__)
 
-# @feedbacks.route("/quiz", methods=["POST"])
-# def quiz_feedback():
-#     print("Reaching!")
-#     data = request.get_json()
-#     num_of_questions = data.get("num_of_questions")
-#     score = data.get("score")
-#     x = 100 * score / num_of_questions
+@feedbacks.route("/quiz", methods=["POST"])
+def quiz_feedback():
+    print("Reaching!")
+    data = request.get_json()
+    num_of_questions = data.get("num_of_questions")
+    score = data.get("score")
+    x = 100 * score / num_of_questions
 
-#     return jsonify({"comment": feedback_quiz(x)}), 200
+    return jsonify({"comment": feedback_quiz(x)}), 200
 
 # @feedbacks.route("/flash", methods=["GET"])
 # def flashcard_feedback():
